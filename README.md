e0ec8f54dfc038a2ee5e2722a7c1c9a04596f9dc
Sergio Florean Chavez <sergio_floreanch@live.com>
2026-04-16 14:03:20 -0600

    adding reusable functions to edit user profile using a pop up form

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Tripleten web_project_around_es
 
+on this first stage
+
 This project is part of a web development learning process focused on connecting JavaScript to an HTML page and working with basic programming concepts. In this stage, an array called initialCards was created, containing six objects with a name and an image link. The project uses a forEach() method to loop through the array and display each card’s name in the browser console, helping to understand how to work with data before manipulating the DOM.
+
+on this second stage
+
+In this stage of the project, we implemented the logic to edit the user profile using a popup form. We selected the necessary DOM elements, including the form, inputs, and profile display fields, and created reusable functions to open and close the modal. We also built a function to pre-fill the form with the current profile information and another handler function to update the profile text on the page when the form is submitted. Finally, we used the submit event listener on the form to capture user input, prevent the default page reload behavior, and dynamically update the profile information in real time.
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+console.log("🚀 index.js cargado correctamente");
 const initialCards = [
   {
     name: "Valle de Yosemite",
@@ -28,3 +29,74 @@ const initialCards = [
 initialCards.forEach((item) => {
   console.log(item.name);
 });
+// boton para abrir el modal de editar perfil
+const editButton = document.querySelector(".profile__edit-button");
+const editPopup = document.querySelector("#edit-popup");
+const closeButton = editPopup.querySelector(".popup__close");
+
+// referencia al formulario para manejar el submit
+const formElement = document.querySelector("#edit-profile-form");
+
+// elementos del formulario de editar perfil
+
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+
+const nameInput = editPopup.querySelector(".popup__input_type_name");
+const descriptionInput = editPopup.querySelector(
+  ".popup__input_type_description",
+);
+
+// funciones para abrir y cerrar el modal de editar perfil
+const openModal = (modal) => {
+  modal.classList.add("popup_is-opened");
+};
+const closeModal = (modal) => {
+  modal.classList.remove("popup_is-opened");
+};
+// eventos para abrir y cerrar el modal de editar perfil
+editButton.addEventListener("click", () => {
+  handleOpenEditModal();
+});
+
+closeButton.addEventListener("click", () => {
+  closeModal(editPopup);
+});
+
+//funcion que solo rellene los campos de entrada del formulario con los valores mostrados en la pagina
+
+const fillProfileForm = () => {
+  console.log("Valor actual nombre:", profileTitle.textContent);
+  console.log("Valor actual descripción:", profileDescription.textContent);
+
+  nameInput.value = profileTitle.textContent;
+  descriptionInput.value = profileDescription.textContent;
+};
+
+// Implementar otra función handleOpenEditModal() que rellenará el formulario y abrirá la ventana emergente, llamando a otras dos funciones existentes.
+const handleOpenEditModal = () => {
+  console.log("Abriendo modal de edición");
+
+  fillProfileForm();
+  openModal(editPopup);
+};
+
+//Implementa la función handleProfileFormSubmit().
+
+const handleProfileFormSubmit = (event) => {
+  event.preventDefault();
+  console.log("Formulario enviado");
+
+  console.log("Valor del input nombre:", nameInput.value);
+  console.log("Valor del input descripción:", descriptionInput.value);
+
+  profileTitle.textContent = nameInput.value;
+  profileDescription.textContent = descriptionInput.value;
+
+  closeModal(editPopup);
+
+  console.log("Perfil actualizado en la página");
+};
+
+//Utiliza el método addEventListener() para detectar cuándo envía el usuario el formulario y llama a la función handleProfileFormSubmit().
+formElement.addEventListener("submit", handleProfileFormSubmit);
